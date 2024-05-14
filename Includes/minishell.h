c74d022282025972f05935d33e69aeb6b3b07be5
Alexander Bello <abello-r@Alex--M3-Pro.local>
2024-05-14 15:24:55 +0200

    Work2

--- Includes/minishell.h
+++ Includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: briveiro <briveiro@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abello-r <abello-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 16:51:19 by abello-r          #+#    #+#             */
-/*   Updated: 2024/05/14 15:22:00 by briveiro         ###   ########.fr       */
+/*   Updated: 2024/05/14 15:24:10 by abello-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,16 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-# define SIMPLE_QUOTE "Error: quotation not closed"
+# define SIMPLE_QUOTE "[ERROR] Simple quote not closed\n"
+# define DOUBLE_QUOTE "[ERROR] Double quote not closed\n"
+
 
 typedef struct s_token
 {
 	char			*content;
 	char			*type;
-	struct s_token	*prev;
-	struct s_token	*next;
+	struct	s_token	*prev;
+	struct	s_token	*next;
 }			t_token;
 
 typedef struct s_data
@@ -39,10 +41,10 @@ typedef struct s_data
 	t_token	*token;
 }			t_data;
 
-void	ft_print_exit(char *str);
 int		ft_is_quotation_closed(char *input, char c);
-void	ft_loop(t_data *data);
+int		ft_loop(t_data *data);
 int		ft_pair_quotation_check(t_data *data);
+void	ft_print_exit(char *str);
 void	parser(t_data *data);
 
 #endif
--- Sources/main.c
+++ Sources/main.c
@@ -4,27 +4,29 @@ int	main(int argc, char **argv, char **envp)
 {
 	t_data	data;
 
-	// printf("'\"''\''");
 	(void) argv;
 	(void) envp;
 	(void) argc;
-	ft_loop(&data);
 
-	return (0);
+	if (ft_loop(&data) == 1)
+		return (1);
+	else
+		return (0);
 }
 
-void	ft_loop(t_data *data)
+int	ft_loop(t_data *data)
 {
 	while (1)
 	{
 		data->input = readline("minishell$ ");
-		if (!data->input)
-			ft_print_exit("exit\n");
-		if (!ft_pair_quotation_check(data))
-			ft_print_exit(SIMPLE_QUOTE);
-		printf("%s\n", data->input);
-		free(data->input);
-		// implementar add_history al final del checker
+		if (!data->input || !ft_pair_quotation_check(data))
+			return (1);
+		else
+		{
+			printf("%s\n", data->input);
+			free(data->input);
+		}
+		// TODO: Implementar [ add_history ] luego de la comprobación de la linea
 	}
 }
 
@@ -47,14 +49,9 @@ int	ft_is_quotation_closed(char *input, char c)
 
 int	ft_pair_quotation_check(t_data *data)
 {
-	// int	count;
-	// return 0 o return con codigo de salida de error
-
-	if ((ft_is_quotation_closed(data->input, '\'') % 2 != 0)
-		|| (ft_is_quotation_closed(data->input, '\"') % 2 != 0))
+	if ((ft_is_quotation_closed(data->input, '\'') % 2 != 0) || (ft_is_quotation_closed(data->input, '\"') % 2 != 0))
 		return (0);
 	if (ft_strchr("<>|;", data->input[ft_strlen(data->input) - 1]))
 		return (0);
 	return (1);
-	// Añadir variable len
 }
