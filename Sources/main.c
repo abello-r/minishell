8213f0d23ecd7ea60157424cac605a54f82db78c
BRI4MZP <riveirodelrio@gmail.com>
2024-05-14 21:02:12 +0200

    Doing some testing on main.c file

--- Sources/main.c
+++ Sources/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abello-r <abello-r@student.42.fr>          +#+  +:+       +#+        */
+/*   By: briveiro <briveiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 18:41:24 by abello-r          #+#    #+#             */
-/*   Updated: 2024/05/14 20:47:53 by abello-r         ###   ########.fr       */
+/*   Updated: 2024/05/14 21:01:54 by briveiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	main(int argc, char **argv, char **envp)
 	(void) envp;
 	(void) argc;
 
-	data_init(envp);
+	data_init(envp); // testing
 
 	if (ft_loop(&data))
 		return (1);
