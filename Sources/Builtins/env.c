2472e2e0724c682b18a058da1cfd1a21c9c81006
Alexander Bello <abello-r@Alex--M3-Pro.local>
2024-05-16 17:33:16 +0200

    Working on fill paths

--- Sources/Builtins/env.c
+++ Sources/Builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: abello-r <abello-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 18:44:38 by abello-r          #+#    #+#             */
-/*   Updated: 2024/05/16 16:12:43 by abello-r         ###   ########.fr       */
+/*   Updated: 2024/05/16 17:26:29 by abello-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,21 @@
 
 void ft_env(t_data *data)
 {
-	int x;
+	if (!data->path) {
+		printf("No path found\n");
+		return ;
+	} else {
+		printf("%s\n", data->path[0]);
+		printf("%s\n", data->path[1]);
+		printf("%s\n", data->path[2]);
+	}
+
+	/*int x;
 
 	x = 0;
-	while (data->envp[x] != NULL)
+	while (data->path[x] != NULL)
 	{
-		printf("%s\n", data->envp[x]);
+		printf("%s\n", data->path[x]);
 		x++;
-	}
+	}*/
 }
--- Sources/envp_parser.c
+++ Sources/envp_parser.c
@@ -6,70 +6,60 @@
 /*   By: abello-r <abello-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 15:59:44 by abello-r          #+#    #+#             */
-/*   Updated: 2024/05/16 16:20:54 by abello-r         ###   ########.fr       */
+/*   Updated: 2024/05/16 17:32:31 by abello-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Includes/minishell.h"
 
-char *ft_allocate_string(const char *str) {
-    size_t len = ft_strlen(str) + 1;
-    char *new_str = malloc(len * sizeof(char));
-    if (!new_str)
-        return NULL;
-    ft_memcpy(new_str, str, len);
-    return new_str;
+char *ft_allocate_string(const char *str)
+{
+	size_t len = ft_strlen(str) + 1;
+	char *new_str = malloc(len * sizeof(char));
+	if (!new_str)
+		return NULL;
+	ft_memcpy(new_str, str, len);
+	return new_str;
 }
 
-void fill_data_envp(char **envp, t_data *data) {
-    int num_env_vars;
-    int max_env_vars;
+void fill_data_envp(char **envp, t_data *data)
+{
+	int num_env_vars;
+	int max_env_vars;
 	int i;
 
 	num_env_vars = 0;
 	max_env_vars = 100;
 	i = 0;
-    char **clean_environments = malloc(max_env_vars * sizeof(char *));
-    if (!clean_environments) 
+	char **clean_environments = malloc(max_env_vars * sizeof(char *));
+	if (!clean_environments)
 		return;
-    while (envp[num_env_vars] != NULL) {
-        num_env_vars++;
-    }
-	while (envp[i] != NULL) {
-	    clean_environments[i] = ft_allocate_string(envp[i]);
-	    if (clean_environments[i] == NULL) {
-			while (i > 0) {
+	while (envp[num_env_vars] != NULL)
+		num_env_vars++;
+	while (envp[i] != NULL)
+	{
+		clean_environments[i] = ft_allocate_string(envp[i]);
+		if (clean_environments[i] == NULL)
+			while (i-- > 0)
 				free(clean_environments[i - 1]);
-				i--;
-			}
-			return;
-	    }
-	    i++;
+		i++;
 	}
-    clean_environments[num_env_vars] = NULL;
+	clean_environments[num_env_vars] = NULL;
 	data->envp = clean_environments;
 }
 
 void fill_path(t_data *data)
 {
-	char    **spl;
-	int     count;
-	char    *tmp;
-	count = 0;
-	while (data->envp[count])
+	int i;
+	char **path_blocks;
+
+	i = 0;
+
+	while (data->envp[i])
 	{
-		if (data->envp[count][0] == 'P' && data->envp[count][1] == 'A'
-			&& data->envp[count][2] == 'T')
-			break ;
-		count++;
+		if (ft_strncmp(data->envp[i], "PATH=", 5) == 0) {
+			path_blocks = ft_split(&data->envp[i][5], ':');
+		}
+		i++;
 	}
-	spl = ft_split(&data->envp[count][5], ':');
-	count = -1;
-	while (spl[++count])
-	{
-		tmp = ft_strjoin(spl[count], "/");
-		free(spl[count]);
-		spl[count] = tmp;
-	}
-	data->path = spl;
 }
