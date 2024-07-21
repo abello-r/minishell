d79c7867aad0b9538e6fd7cffdda9789557dcf1c
Alexander Bello <abello-r@Alex--M3-Pro.local>
2024-07-21 19:36:50 +0200

    Add comments to exit file

--- Sources/Builtins/exit.c
+++ Sources/Builtins/exit.c
@@ -6,13 +6,21 @@
 /*   By: abello-r <abello-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/21 11:58:43 by abello-r          #+#    #+#             */
-/*   Updated: 2024/07/21 11:59:06 by abello-r         ###   ########.fr       */
+/*   Updated: 2024/07/21 19:36:19 by abello-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../Includes/minishell.h"
 
-void	ft_exit(t_data *data)
+/* Function to exit the current process:
+
+- Exits with the arg if it exists and is numeric, otherwise exits with 0
+
+- Returns error if there is more than 1 arg (does not exit)
+
+- Returns error if there is a non-numeric arg (exits) */
+
+void	ft_exit(t_data *data) // TO DO:
 {
 	(void)data;
 	exit(0);
