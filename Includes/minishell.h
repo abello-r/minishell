7c20881db54545e08aa7dc6e9dbcf8387d141a31
Paula <pausanpi1606@gmail.com>
2024-11-12 15:53:06 +0100

    pipes y redirecciones

--- Includes/minishell.h
+++ Includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abello-r <abello-r@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pausanch <pausanch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/21 03:46:27 by briveiro          #+#    #+#             */
-/*   Updated: 2024/07/21 22:59:27 by abello-r         ###   ########.fr       */
+/*   Updated: 2024/11/12 14:37:29 by pausanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@
 # include	<readline/readline.h>
 # include	<readline/history.h>
 # include	<sys/wait.h>
+# include	<sys/types.h>
+# include	<fcntl.h>
 
 # define SIMPLE_QUOTE "[ERROR] Simple quote not closed\n"
 # define DOUBLE_QUOTE "[ERROR] Double quote not closed\n"
