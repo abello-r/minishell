5db13bcab03435955be6cc416edfe324dc340928
abello-r <alexanderbellor18@gmail.com>
2025-01-14 18:55:22 +0100

    Norminette on utils files OK

--- Sources/utils_2.c
+++ Sources/utils_2.c
@@ -26,10 +26,11 @@ int	ft_is_empty(char *str)
 	return (1);
 }
 
-char *ft_strncpy(char *dest, const char *src, size_t n)
+char	*ft_strncpy(char *dest, const char *src, size_t n)
 {
-	size_t i = 0;
+	size_t	i;
 
+	i = 0;
 	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
@@ -40,5 +41,28 @@ char *ft_strncpy(char *dest, const char *src, size_t n)
 		dest[i] = '\0';
 		i++;
 	}
-	return dest;
+	return (dest);
+}
+
+void	ft_process_export_args(t_data *data)
+{
+	char	*desired_new_env;
+
+	desired_new_env = NULL;
+	while (data->token->next != NULL && data->token->next->content != NULL \
+		&& ft_strcmp(data->token->next->type, "ARG") == 0)
+	{
+		if (data->token->next && data->token->next->content \
+			&& ft_strchr(data->token->next->content, '='))
+		{
+			desired_new_env = ft_strdup(data->token->next->content);
+		}
+		else if (data->token->next && data->token->next->content)
+		{
+			desired_new_env = ft_strdup(data->token->next->content);
+		}
+		ft_args_export_iterator(data, desired_new_env);
+		free(desired_new_env);
+		data->token = data->token->next;
+	}
 }
